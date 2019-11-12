672e58e9e40742f7f91d3def3891ca2769bf7b6e
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-11-12 15:55:54 -0600

    Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,17 +1,19 @@
 require 'date'
 
-start_date = Date.new(2019,1,1)
-final_date = Date.new(2019,9,1)
-
 def date_list(start_date, final_date, day)
   month = final_date.mon
+  month_numbers = start_date.mon
   new_dates = []
-  i=1
-  while (i <= month)
-    new_dates.push(Date.new(2019,i,day).to_s)
-    i+=1
+
+  while (month_numbers <= month)
+    new_dates.push(Date.new(2019, month_numbers, day).strftime('%d %b %Y'))
+    month_numbers += 1
   end
+  
   new_dates
 end
 
-date_list(start_date, final_date, 8)
+start_date = Date.new(2019, 9, 1)
+final_date = Date.new(2019, 10, 1)
+day = 8
+date_list(start_date, final_date, day)
