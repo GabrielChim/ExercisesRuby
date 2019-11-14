0f2d5a2404a067224ee27e0744584e7dafb50d41
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-11-14 17:47:41 -0600

    new corrections

--- clean_code_2.rb
+++ clean_code_2.rb
@@ -1,6 +1,6 @@
 def longname
   name = fullname
   return name unless organization_id
-  return name unless Organization.lookup(id: organization_id)
-  name += "(#{organization.name})"
+  organization = Organization.lookup(id: organization_id)
+  return name += "(#{organization.name})" if organization
 end
