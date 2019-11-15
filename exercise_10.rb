39bd3584a589298552c6ac27a270959b7ea529ff
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-11-15 15:00:15 -0600

    variables names

--- exercise_10.rb
+++ exercise_10.rb
@@ -1,18 +1,18 @@
-def product_rigth_array (digits_array)
+def sum_list (list_numbers)
   sum_numbers = 0
-
-  digits_array.each_index do |item|
-    digit = ""
-    digits_array[item].to_s.chars.reverse_each do |number|
-      break number if number == "0"
-      digit += number
+  
+  list_numbers.each_index do |digit|
+    digits = ""
+    list_numbers[digit].to_s.chars.reverse_each do |number|
+      break if number == "0"
+      digits += number
     end
-    sum_numbers += digit.reverse.to_i
+    sum_numbers += digits.reverse.to_i
   end
-
+  
   sum_numbers
 end
 
-numbers = [1200000265, 1000002360, 1670000800, 1000006002, 1000000233]
+list_numbers = [1200000265, 1000002360, 1670000800, 1000006002, 1000000233]
 
-product_rigth_array(numbers)
+sum_list(list_numbers)
