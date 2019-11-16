a1ff09808fa9954113c40ff635c4eee9f3b80c58
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-11-15 18:32:34 -0600

    Exercise_11_chocolate
    corrections names variables

---
+++ exercise_11.rb
@@ -0,0 +1,33 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the birthday function below.
+def birthday(numbers_list, expected_sum, length)
+  new_list = numbers_list.each_cons(length).to_a
+  sum_result = 0
+  new_list.each do |item|
+     sum_result += 1 if item.reduce(:+).eql?(expected_sum)
+  end
+  sum_result
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = gets.strip.to_i
+
+s = gets.rstrip.split.map(&:to_i)
+
+dm = gets.rstrip.split
+
+d = dm[0].to_i
+
+m = dm[1].to_i
+
+result = birthday s, d, m
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
