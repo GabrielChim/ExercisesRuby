7a72b0cf6282737e78fec68479e456b89edf4176
GabrielChim <gabriel.chim.pech@gmail>
2019-11-16 18:20:10 -0600

    variable_name

--- exercise_11.rb
+++ exercise_11.rb
@@ -4,8 +4,8 @@ require 'json'
 require 'stringio'
 
 # Complete the birthday function below.
-def birthday(list_numbers, expected_sum, length)
-  new_list = list_numbers.each_cons(length).to_a
+def birthday(numbers_list, expected_sum, length)
+  new_list = numbers_list.each_cons(length).to_a
   sum_result = 0
   new_list.each do |item|
      sum_result += 1 if item.reduce(:+).eql?(expected_sum)
