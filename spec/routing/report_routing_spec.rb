913a4a248d7cb2850f8878953552358cc61a48a3
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-12-18 14:18:41 -0600

    test rspec .gitignore

---
+++ spec/routing/report_routing_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe ReportController, type: :routing do
+  describe "routing" do
+    it "routes to #info" do
+      expect(get: "/reports/info").to route_to("report#info")
+    end
+  end
+end
