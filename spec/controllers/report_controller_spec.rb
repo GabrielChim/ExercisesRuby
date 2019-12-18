248cb415fc3eb0276e7a9d41fdf5a0c4ca799249
Gabriel chim <gabriel.chim.pech@gmail.com>
2019-12-18 16:16:12 -0600

    gitignore

--- spec/controllers/report_controller_spec.rb
+++ spec/controllers/report_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ReportController, type: :controller do
+
   describe "GET #info" do
     it "returns http info success" do
       get :info
       expect(response).to have_http_status(:success)
     end
   end
+  
 end
