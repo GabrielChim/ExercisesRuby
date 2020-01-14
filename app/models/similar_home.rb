a4064ef4d293f1e8bf6dcca93ea2d8a0af98e425
GabrielChim <gabriel.chim.pech@gmail.com>
2020-01-13 18:04:00 -0600

    clean code

--- app/models/similar_home.rb
+++ app/models/similar_home.rb
@@ -1,37 +1,33 @@
 class SimilarHome  
-  def self.list_ids
+  def self.list_similar_homes
     ids_owners = Owner.pluck(:id)
     ids_homes_equals = []
     homes_equals = []
 
     ids_owners.map do |id|
-      homes = Home.where(owner: id, 
-              home_master_id: nil, 
-              _status: :published)
-      if homes.count > 1
-        homes.map do |home|
-          id_data_homes = Home.where(owner: home.owner.id, 
-                      total_amount: home.total_amount,
-                      location: home.location,
-                      home_master_id: nil).pluck(:id)
-          if id_data_homes.count > 1
-            ids_homes_equals.push(id_data_homes)
-          end
-        end
+      homes = Home.where(owner: id, home_master_id: nil, _status: :published)
+      next if homes.size < 2
+      
+      homes.map do |home|
+        id_data_homes = Home.where(owner: home.owner.id, 
+                    total_amount: home.total_amount,
+                    location: home.location,
+                    home_master_id: nil).pluck(:id)
+        next if id_data_homes.size < 2
+        ids_homes_equals.push(id_data_homes)
       end
     end
 
     ids_homes_equals.uniq.map do |id_home|
       id_home.map do |id|
-        home = Home.where(id: id)
-        full_name = "#{home[0].owner.user.name} #{home[0].owner.user.last_name}"
-        email = home[0].owner.user.email
-        id_depa = home[0].id
-        precio_depa = home[0].total_amount
-        status_depa = home[0].status
-        location_depa = home[0].location
-
-        puts "#{full_name},#{email},#{id_depa},#{precio_depa},#{status_depa},#{location_depa}"
+        home = Home.find(id: id)
+        puts "#{home.owner.user.name} #{home.owner.user.last_name}," \
+              "#{home.owner.user.email},"  \
+              "#{home.id}," \
+              "#{home.home_master_id}," \
+              "#{home.total_amount}," \
+              "#{home.status}," \
+              "#{home.location}"
       end
     end
   end
